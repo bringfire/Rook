3ae30a4f0db6f119b2e8818b71702cdffeb06765
bringfire <bringfire@users.noreply.github.com>
2026-05-11 13:20:18 -0400

    release: bump versions to 1.5.5

--- src/RookNative/RookNativePlugin.cpp
+++ src/RookNative/RookNativePlugin.cpp
@@ -249,7 +249,7 @@ static CRookNativePlugin thePlugIn;
 // --- Plugin Implementation ---
 
 CRookNativePlugin::CRookNativePlugin()
-    : m_plugin_version(L"1.5.4")
+    : m_plugin_version(L"1.5.5")
 {
     // The constructor is called before OnLoadPlugIn.
     // Do NOT access Rhino SDK objects here.
--- src/RookNative/RookServer.cpp
+++ src/RookNative/RookServer.cpp
@@ -1744,7 +1744,7 @@ void CRookServer::WriteDiscoveryFile()
         info["pluginType"] = "native";
         info["processId"] = ::GetCurrentProcessId();
         info["startTime"] = ts.str();
-        info["pluginVersion"] = "1.5.4";
+        info["pluginVersion"] = "1.5.5";
         info["rhinoInside"] = CRookNativePlugin::IsRhinoInside();
         const auto ghRoutes = GetNativeGrasshopperRoutes();
         const bool callbackBridgeReady = Rook::Handlers::HasGrasshopperBridgeRegistration();
